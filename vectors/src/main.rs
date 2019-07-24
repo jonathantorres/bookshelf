ce25f2f5f2d22c11321bdd51d24d6f18c0bdeccb
Jonathan Torres <jonathantorres41@gmail.com>
2019-07-24 00:23:01 -0400

    Vectors

---
+++ vectors/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+}
+
+fn reading_vectors() {
+    let v = vec![1,2,3,4];
+}
+
+fn creating_vectors() {
+    let mut v = Vec::new();
+    v.push(1);
+    v.push(2);
+    v.push(3);
+    v.push(4);
+
+    let v2:Vec<i32> = Vec::new();
+    let v3 = vec![1,2,3,4];
+}
