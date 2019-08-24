5b80b9e02678162c2208d54dbd53cb59044bcc2f
Jonathan Torres <jonathantorres41@gmail.com>
2019-08-24 12:07:25 -0400

    Solve exercises

--- unit2/module2-what-is-ownership/08-30-ownership-exercise/src/main.rs
+++ unit2/module2-what-is-ownership/08-30-ownership-exercise/src/main.rs
@@ -2,13 +2,17 @@ fn main() {
     let s = String::from("book");
 
     // Add code here that calls the pluralize function
+    let plural = pluralize(s.clone());
 
     println!(
         "I have one {}, you have two {}",
         s,
-        you_add_something_here,
+        plural,
     );
 }
 
 // Add appropriate parameters, return values, and implementation to this function
-fn pluralize() {}
+fn pluralize(mut w: String) -> String {
+    w.push_str("s");
+    w
+}
