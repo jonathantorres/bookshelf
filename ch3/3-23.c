54686c1a2d48c53c713033d71a26e589643c19be
Jonathan Torres <jonathantorres41@gmail.com>
2019-11-17 14:59:38 -0500

    Add answer for 3-23

---
+++ ch3/3-23.c
@@ -0,0 +1,242 @@
+/**
+ * 3-23 - Implement an algorithm to reverse a linked list. Now do it without recursion.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct list_node {
+    void *value;
+    struct list_node *next;
+} list_node;
+
+typedef struct list {
+    list_node *first;
+} list;
+
+// Macro usage:
+// SLLIST_FOREACH(list) {
+    // your code here
+    // you can use the variable "cur"
+    // inside of it that references the current item of the list
+// }
+#define LIST_FOREACH(_list) \
+    for (list_node *cur = (_list)->first; cur != NULL; cur = cur->next)
+
+list_node *create_node(void *value);
+void destroy_node(list_node *node);
+list *list_new();
+void list_clear(list *_list);
+void list_destroy(list *_list);
+void list_push(list *_list, void *value);
+void list_reverse(list *_list);
+void list_reverse_rec(list *_list);
+int list_length(list *_list);
+
+void list_reverse_node(list_node *cur_node, list_node **prev_node)
+{
+    if (!cur_node) {
+        return;
+    }
+    list_node *next_node = cur_node->next;
+    cur_node->next = *prev_node;
+    *prev_node = cur_node;
+    cur_node = next_node;
+
+    list_reverse_node(cur_node, prev_node);
+}
+
+void list_reverse_rec(list *_list)
+{
+    if (!_list) {
+        return;
+    }
+    if (list_length(_list) == 0) {
+        return;
+    }
+
+    list_node *cur_node = _list->first;
+    list_node *prev_node = NULL;
+    list_reverse_node(cur_node, &prev_node);
+    _list->first = prev_node;
+}
+
+void list_reverse(list *_list)
+{
+    if (!_list) {
+        return;
+    }
+    if (list_length(_list) == 0) {
+        return;
+    }
+    list_node *cur_node = _list->first;
+    list_node *prev_node = NULL;
+    list_node *next_node = NULL;
+
+    while (cur_node != NULL) {
+        next_node = cur_node->next;
+        cur_node->next = prev_node;
+        prev_node = cur_node;
+        cur_node = next_node;
+    }
+    _list->first = prev_node;
+}
+
+list_node *create_node(void *value)
+{
+    list_node *node = malloc(sizeof(list_node));
+
+    if (!node) {
+        return NULL;
+    }
+
+    node->next = NULL;
+    node->value = value;
+
+    return node;
+}
+
+void destroy_node(list_node *node)
+{
+    if (!node) {
+        return;
+    }
+
+    node->next = NULL;
+    node->value = NULL;
+
+    free(node);
+}
+
+list *list_new()
+{
+    list *new_list = malloc(sizeof(list));
+
+    if (!new_list) {
+        return NULL;
+    }
+
+    new_list->first = NULL;
+
+    return new_list;
+}
+
+void list_clear(list *_list)
+{
+    if (_list->first != NULL) {
+        list_node *current_node = _list->first;
+        list_node *prev_node = NULL;
+
+        while (current_node->next != NULL) {
+            prev_node = current_node;
+            current_node = current_node->next;
+
+            if (prev_node) {
+                destroy_node(prev_node);
+            }
+        }
+
+        if (current_node) {
+            destroy_node(current_node);
+        }
+
+        _list->first = NULL;
+    }
+}
+
+void list_destroy(list *_list)
+{
+    if (!_list) {
+        return;
+    }
+
+    list_clear(_list);
+    free(_list);
+}
+
+// get the length of the list
+int list_length(list *_list)
+{
+    if (!_list) {
+        return -1;
+    }
+
+    int length = 0;
+
+    if (_list->first != NULL) {
+        list_node *current_node = _list->first;
+        length++;
+
+        while (current_node->next != NULL) {
+            current_node = current_node->next;
+            length++;
+        }
+    }
+
+    return length;
+}
+
+// insert at the end
+void list_push(list *_list, void *value)
+{
+    if (!_list) {
+        return;
+    }
+
+    list_node *new_node = create_node(value);
+
+    // list is empty, this is the first element
+    if (_list->first == NULL) {
+        _list->first = new_node;
+        return;
+    }
+
+    list_node *current_node = _list->first;
+
+    while (current_node->next != NULL) {
+        current_node = current_node->next;
+    }
+
+    current_node->next = new_node;
+}
+
+int main(void)
+{
+    list *names = list_new();
+    list_push(names, strdup("Jonathan 1"));
+    list_push(names, strdup("Jonathan 2"));
+    list_push(names, strdup("Jonathan 3"));
+    list_push(names, strdup("Jonathan 4"));
+
+    // reverse normally
+    LIST_FOREACH(names) {
+        printf("- %s\n", (char*)cur->value);
+    }
+    puts("");
+    list_reverse(names);
+    LIST_FOREACH(names) {
+        printf("- %s\n", (char*)cur->value);
+    }
+    list_destroy(names);
+    puts("");
+
+    list *more_names = list_new();
+    list_push(more_names, strdup("John 1"));
+    list_push(more_names, strdup("John 2"));
+    list_push(more_names, strdup("John 3"));
+    list_push(more_names, strdup("John 4"));
+
+    // reverse recursively
+    LIST_FOREACH(more_names) {
+        printf("- %s\n", (char*)cur->value);
+    }
+    puts("");
+    list_reverse_rec(more_names);
+    LIST_FOREACH(more_names) {
+        printf("- %s\n", (char*)cur->value);
+    }
+    list_destroy(more_names);
+
+    return 0;
+}
