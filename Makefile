24bd3f4c3625e2e5c7baa2660d3761ef68cd01eb
Jonathan Torres <jonathantorres41@gmail.com>
2020-02-20 00:50:10 -0500

    Add exercises from 1-3 to 1-5

--- Makefile
+++ Makefile
@@ -6,6 +6,15 @@ CFLAGS := gcc -std=c11 -Wall -Wextra
 1-2: ch1/1-2.c
 	$(CFLAGS) ch1/1-2.c -o bin/1-2
 	./bin/1-2
+1-3: ch1/1-3.c
+	$(CFLAGS) ch1/1-3.c -o bin/1-3
+	./bin/1-3
+1-4: ch1/1-4.c
+	$(CFLAGS) ch1/1-4.c -o bin/1-4
+	./bin/1-4
+1-5: ch1/1-5.c
+	$(CFLAGS) ch1/1-5.c -o bin/1-5
+	./bin/1-5
 
 clean:
 	rm -f ./*.o ch*/*.h.gch
---
+++ ch1/1-4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper  = 300;
+    step = 20;
+    celsius = lower;
+
+    printf("Celsius\t\tFahrenheit\n");
+    while (celsius <= upper) {
+        fahr = (((celsius*9.0)/5) + 32);
+        printf("%3.0f\t\t%6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
