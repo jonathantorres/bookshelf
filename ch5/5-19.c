78c8f580a8e974e791362c1084f3076281abf3fc
Jonathan Torres <jonathantorres41@gmail.com>
2020-02-29 18:33:53 -0500

    Add 5-18 to 5-20

---
+++ ch5/5-19.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXTOKEN 100
+enum { NAME, PARENS, BRACKETS };
+enum { NO, YES };
+
+int tokentype;
+int pretoken = NO;
+int posttoken(void);
+int gettoken(void);
+char token[MAXTOKEN];
+char out[1000];
+
+int main(void)
+{
+    int type;
+    char temp[MAXTOKEN];
+    void ungetch(int c);
+
+    while (gettoken() != EOF) {
+        strcpy(out, token);
+        while ((type = gettoken()) != '\n') {
+            if (type == PARENS || type == BRACKETS) {
+                strcat(out, token);
+            } else if (type == '*') {
+                if ((type = posttoken()) == PARENS || type == BRACKETS) {
+                    sprintf(temp, "(*%s)", out);
+                } else {
+                    sprintf(temp, "*%s", out);
+                }
+                strcpy(out, temp);
+            } else if (type == NAME) {
+                sprintf(temp, "%s %s", token, out);
+                strcpy(out, temp);
+            } else {
+                printf("invalid input at %s\n", token);
+            }
+        }
+        printf("%s\n", out);
+    }
+    return 0;
+}
+
+int gettoken(void)
+{
+    int c, getch(void);
+    void ungetch(int);
+    char *p = token;
+
+    if (pretoken == YES) {
+        pretoken = NO;
+        return tokentype;
+    }
+    while ((c = getch()) == ' ' || c == '\t') {
+    }
+    if (c == '(') {
+        if ((c = getch()) == ')') {
+            strcpy(token, "()");
+            return tokentype = PARENS;
+        } else {
+            ungetch(c);
+            return tokentype = '(';
+        }
+    } else if(c == '[') {
+        for (*p++ = c; (*p++ = getch()) != ']';) {
+        }
+        *p = '\0';
+        return tokentype = BRACKETS;
+    } else if (isalpha(c)) {
+        for (*p++ = c; isalnum(c = getch());) {
+            *p++ = c;
+        }
+        *p = '\0';
+        ungetch(c);
+        return tokentype = NAME;
+    } else {
+        return tokentype = c;
+    }
+}
+
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE) {
+        printf("ungetch: too many characters\n");
+    } else {
+        buf[bufp++] = c;
+    }
+}
+
+int posttoken(void)
+{
+    int nexttype;
+    nexttype = gettoken();
+    pretoken = YES;
+    return nexttype;
+}
