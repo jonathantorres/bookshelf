558637eafc0ae65f4a5a8d1a77b0cc75cb5ae881
Jonathan Torres <jonathantorres41@gmail.com>
2020-04-06 23:45:16 -0400

    Work on stat

--- ch39/stat.c
+++ ch39/stat.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <sys/stat.h>
 
 int main(int argc, char const *argv[])
 {
-    struct stat st;
-    int rc = stat("answers.md", &st);
-    // assert(rc == 0);
-    printf("rc=%d\n", rc);
-    printf("ino=%ld\n", st.st_ino);
+    if (argc < 2) {
+        puts("usage: ./stat filename");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 1; i < argc; i++) {
+        struct stat st;
+        int rc = stat(argv[i], &st);
+        assert(rc == 0);
+        printf("rc=%d\n", rc);
+        printf("ino=%ld\n", st.st_ino);
+        printf("filename given: %s\n", argv[i]);
+    }
     return 0;
 }
