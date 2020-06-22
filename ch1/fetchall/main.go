980cd711570ff3482df32c439b093a0b5ca25ba5
Jonathan Torres <jonathantorres41@gmail.com>
2020-06-21 23:50:31 -0400

    Add 1.10, 1.11 and 1.12

---
+++ ch1/fetchall/main.go
@@ -0,0 +1,47 @@
+package main
+
+// Exercise 1.10
+// Exercise 1.11
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	var output io.Writer
+	output, err = os.Create("out.txt")
+	if err != nil {
+		output = ioutil.Discard
+	}
+	nbytes, err := io.Copy(output, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
