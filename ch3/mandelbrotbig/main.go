02597a10a2d57df9019d532c1099c6ad9b5b5b30
Jonathan Torres <jonathantorres41@gmail.com>
2020-06-24 19:21:29 -0400

    Exercises 3.5 through 3.9

---
+++ ch3/mandelbrotbig/main.go
@@ -0,0 +1,56 @@
+package main
+
+// Exercise 3.8
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math"
+	"math/big"
+	"os"
+)
+
+const (
+	xmin, ymin, xmax, ymax = -2, -2, +2, +2
+	width, height          = 1024, 1024
+)
+
+func main() {
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			img.Set(px, py, mandelbrotBigFloat(z))
+		}
+	}
+	png.Encode(os.Stdout, img)
+}
+
+func mandelbrotBigFloat(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+	zR := (&big.Float{}).SetFloat64(real(z))
+	zI := (&big.Float{}).SetFloat64(imag(z))
+	var vR, vI = &big.Float{}, &big.Float{}
+	for i := uint8(0); i < iterations; i++ {
+		vR2, vI2 := &big.Float{}, &big.Float{}
+		vR2.Mul(vR, vR).Sub(vR2, (&big.Float{}).Mul(vI, vI)).Add(vR2, zR)
+		vI2.Mul(vR, vI).Mul(vI2, big.NewFloat(2)).Add(vI2, zI)
+		vR, vI = vR2, vI2
+		squareSum := &big.Float{}
+		squareSum.Mul(vR, vR).Add(squareSum, (&big.Float{}).Mul(vI, vI))
+		if squareSum.Cmp(big.NewFloat(4)) == 1 {
+			switch {
+			case i > 50:
+				return color.RGBA{100, 0, 0, 255}
+			default:
+				logScale := math.Log(float64(i)) / math.Log(float64(iterations))
+				return color.RGBA{0, 0, 255 - uint8(logScale*255), 255}
+			}
+		}
+	}
+	return color.Black
+}
