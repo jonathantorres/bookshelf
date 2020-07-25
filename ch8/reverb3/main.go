b663126aa12195cc39bc56c11c4c76a648203148
Jonathan Torres <jonathantorres41@gmail.com>
2020-07-25 11:03:33 -0400

    Add 8.8

---
+++ ch8/reverb3/main.go
@@ -0,0 +1,65 @@
+package main
+
+// Exercise 8.8
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:9999")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func echo(c net.Conn, shout string, delay time.Duration, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	wg := &sync.WaitGroup{}
+	defer func() {
+		wg.Wait()
+		c.Close()
+	}()
+	lines := make(chan string)
+	go func(r io.Reader, lines chan<- string) {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			lines <- s.Text()
+		}
+	}(c, lines)
+	timeout := 2 * time.Second
+	timer := time.NewTimer(2 * time.Second)
+	for {
+		select {
+		case line := <-lines:
+			timer.Reset(timeout)
+			wg.Add(1)
+			go echo(c, line, 1*time.Second, wg)
+		case <-timer.C:
+			return
+		}
+	}
+}
