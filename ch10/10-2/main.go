bfea728fdc8f3aeaf3c1e36f43c20204271aff61
Jonathan Torres <jonathantorres41@gmail.com>
2021-04-24 21:15:36 -0400

    Finish 10-2

--- ch10/10-2/main.go
+++ ch10/10-2/main.go
@@ -4,13 +4,28 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/jonathantorres/gopl/ch10/10-2/arch/tar"
+	"github.com/jonathantorres/gopl/ch10/10-2/arch"
+	_ "github.com/jonathantorres/gopl/ch10/10-2/arch/tar"
+	_ "github.com/jonathantorres/gopl/ch10/10-2/arch/zip"
 )
 
 func main() {
-	f, err := tar.Open("file.tar")
+	z, err := arch.Open("Consolas.zip")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
+	}
+	for _, f := range z.Files {
+		fmt.Println(f)
+	}
+	fmt.Println()
+
+	z2, err := arch.Open("go1.13.5.src.tar")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+	for _, f := range z2.Files {
+		fmt.Println(f)
 	}
 }
