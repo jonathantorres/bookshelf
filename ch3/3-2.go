8eb064a4549be5a477b437137a17f2519904ac81
Jonathan Torres <jonathantorres41@gmail.com>
2021-10-19 22:16:11 -0400

    Fixes on chapter 3 programs

--- ch3/3-2.go
+++ ch3/3-2.go
@@ -2,84 +2,77 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
+	"io"
 	"os"
 )
 
 const maxLine = 1000
 
 func main() {
-	line2 := make([]byte, maxLine)
-	line3 := make([]byte, maxLine)
+	r := bufio.NewReader(os.Stdin)
 
 	for {
-		line, l := getLine(maxLine)
+		line, err := r.ReadBytes(byte('\n'))
+		if err != nil {
+			if err != io.EOF {
+				fmt.Printf("error: %s\n", err)
+			}
+			break
+		}
+		l := len(line)
 		if l == 0 {
 			break
 		}
-		escape(line2, line)
-		rEscape(line3, line2)
+		line2 := escape(line)
+		line3 := rEscape(line2)
 		fmt.Printf("%s\n", string(line2))
 		fmt.Printf("%s\n", string(line3))
 	}
 }
 
-func escape(s []byte, t []byte) {
-	for i, j := 0, 0; t[i] != 0; i, j = i+1, j+1 {
-		switch rune(t[i]) {
+func escape(line []byte) []byte {
+	var buf bytes.Buffer
+	for _, b := range line {
+		switch rune(b) {
 		case '\n':
-			s[j] = '\\'
-			j++
-			s[j] = 'n'
+			buf.WriteByte(byte('\\'))
+			buf.WriteByte(byte('n'))
 			break
 		case '\t':
-			s[j] = '\\'
-			j++
-			s[j] = 't'
+			buf.WriteByte(byte('\\'))
+			buf.WriteByte(byte('t'))
 			break
 		default:
-			s[j] = t[i]
+			buf.WriteByte(b)
 			break
 		}
 	}
+	return buf.Bytes()
 }
 
-func rEscape(s []byte, t []byte) {
-	for i, j := 0, 0; t[i] != 0; i, j = i+1, j+1 {
-		switch rune(t[i]) {
+func rEscape(line []byte) []byte {
+	var buf bytes.Buffer
+	var esc bool
+	for i, b := range line {
+		switch rune(b) {
 		case '\\':
-			if t[i+1] == 't' {
-				s[j] = '\t'
-				i++
-			} else if t[i+1] == 'n' {
-				s[j] = '\n'
-				i++
-			} else {
-				s[j] = t[i]
+			if line[i+1] == 't' {
+				buf.WriteByte(byte('\t'))
+				esc = true
+			} else if line[i+1] == 'n' {
+				buf.WriteByte(byte('\n'))
+				esc = true
 			}
 			break
 		default:
-			s[j] = t[i]
+			if !esc {
+				buf.WriteByte(b)
+			}
+			esc = false
 			break
 		}
 	}
-}
-
-func getLine(lim int) ([]byte, int) {
-	var c byte
-	var i int
-	s := make([]byte, lim)
-	r := bufio.NewReader(os.Stdin)
-	for i = 0; i < lim-1; i++ {
-		c, err := r.ReadByte()
-		if rune(c) == '\n' || err != nil {
-			break
-		}
-		s[i] = c
-	}
-	if rune(c) == '\n' {
-		s[i] = c
-		i++
-	}
-	return s, i
+	return buf.Bytes()
 }
