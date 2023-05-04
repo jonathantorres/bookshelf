6a342c571b8545d6ab4a3d74e9f72486a0323e52
Jonathan Torres <jonathantorres41@gmail.com>
2023-05-04 08:25:53 -0400

    csapp: fix code style on show-bytes

--- csapp/code/data/show-bytes.c
+++ csapp/code/data/show-bytes.c
@@ -10,7 +10,7 @@ typedef unsigned char *byte_pointer;
 void show_bytes(byte_pointer start, size_t len) {
     size_t i;
     for (i = 0; i < len; i++)
-	printf(" %.2x", start[i]);    //line:data:show_bytes_printf
+        printf(" %.2x", start[i]);    //line:data:show_bytes_printf
     printf("\n");
 }
 
