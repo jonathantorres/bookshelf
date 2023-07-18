cfba926cf3632bbb82c2c5c232081fb88ea6f520
Jonathan Torres <jonathantorres41@gmail.com>
2023-07-18 00:33:45 -0400

    csapp: Add from 3.72 to 3.75

---
+++ csapp/ch03/3.73.c
@@ -0,0 +1,23 @@
+typedef enum { NEG, ZERO, POS, OTHER } range_t;
+
+range_t find_range(float x)
+{
+    __asm__("vxorps %xmm1, %xmm1, %xmm1\n\t"
+            "vucomiss %xmm1, %xmm0\n\t"
+            "jp .P\n\t"
+            "ja .A\n\t"
+            "jb .B\n\t"
+            "je .E\n\t"
+            ".A:\n\t"
+            "movl $2, %eax\n\t"
+            "jmp .Done\n\t"
+            ".B:\n\t"
+            "movl $0, %eax\n\t"
+            "jmp .Done\n\t"
+            ".E:\n\t"
+            "movl $1, %eax\n\t"
+            "jmp .Done\n\t"
+            ".P:\n\t"
+            "movl $3, %eax\n\t"
+            ".Done:\n\t");
+}
