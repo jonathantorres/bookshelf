5fb2e7425d7182aba57baf7fa1c0713a5207f35b
Jonathan Torres <jonathantorres41@gmail.com>
2023-09-04 11:34:35 -0400

    csapp: Add 6.7, 6.8 and 6.9

---
+++ csapp/ch06/6.7.c
@@ -0,0 +1,12 @@
+int sumarray3d(int a[N][N][N])
+{
+    int i, j, k, sum = 0;
+
+    for (k = 0; k < N; k++) {
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
+                sum += a[k][i][j];
+            }
+        }
+    }
+}
