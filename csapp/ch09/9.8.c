38508ec1659baa3fc6827caa84c7b72abe449f41
Jonathan Torres <jonathantorres41@gmail.com>
2023-10-15 18:35:38 -0400

    csapp: add from 9.1 to 9.10

---
+++ csapp/ch09/9.8.c
@@ -0,0 +1,13 @@
+// not tested
+static void *find_fit(size_t asize)
+{
+    void *bp = NULL;
+
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+            return bp;
+        }
+    }
+
+    return NULL;
+}
