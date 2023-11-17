36e5c7a3afafdd4a6086cbaaf8c5ae2ecd365beb
Jonathan Torres <jonathantorres41@gmail.com>
2023-11-16 23:34:15 -0500

    sicp-js: update 2.36

--- sicp-js/ch2/2.36.js
+++ sicp-js/ch2/2.36.js
@@ -8,14 +8,10 @@ const l = list(list(1, 2, 3), list(4, 5, 6), list(7, 8, 9), list(10, 11, 12));
 display_list(accumulate_n(plus, 0, l));
 
 function accumulate_n(op, init, seqs) {
-    if (is_null(head(seqs))) {
-        return null;
-    }
-
-    return pair(
-        accumulate(op, init, map(sl => head(sl), seqs)),
-        accumulate_n(op, init, map(sl => tail(sl), seqs))
-    );
+    return is_null(head(seqs))
+        ? null
+        : pair(accumulate(op, init, map(head, seqs)),
+               accumulate_n(op, init, map(tail, seqs)));
 }
 
 function plus(x, y) {
