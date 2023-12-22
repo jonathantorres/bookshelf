bd6925a50ecaa8c216a037d00d66d8d2560f95d3
Jonathan Torres <jonathantorres41@gmail.com>
2023-12-22 03:40:48 -0500

    sicp-js: many changes on 2.90

--- sicp-js/ch2/2.90.js
+++ sicp-js/ch2/2.90.js
@@ -8,49 +8,77 @@ const operation_table = make_table();
 const get = operation_table("lookup");
 const put = operation_table("insert");
 
-install_javascript_number_package();
-install_sparse_term_list_package();
-install_dense_term_list_package();
+install_sparse_package();
+install_dense_package();
 install_polynomial_package();
 
-const p1 = make_polynomial("x",
-                list(list(2, make_javascript_number(4)),
-                list(1, make_javascript_number(3)),
-                list(0, make_javascript_number(7))));
-const p2 = make_polynomial("x",
-                list(list(2, make_javascript_number(5)),
-                list(1, make_javascript_number(2)),
-                list(0, make_javascript_number(10))));
+const p1 = make_sparse_poly("x", list(list(100,1), list(2, 2), list(0, 1)));
+const p2 = make_dense_poly("x", list(0, 3, 4, 2));
 
-// TODO: Fix representation for these new added types of term lists
-// display_list(add(p1, p2));
+display_list(add(p1, p1));
+display_list(mul(p1, p1));
+display_list(add(p2, p2));
+display_list(mul(p2, p2));
 
-const the_empty_termlist = null;
-function is_empty_termlist(term_list) {
-    return is_null(term_list);
+function make_sparse_poly(variable, terms) {
+    return get("make_sparse", "polynomial")(variable, terms);
 }
 
-function make_term(order, coeff) {
-    return list(order, coeff);
+function make_dense_poly(variable, terms) {
+    return get("make_dense", "polynomial")(variable, terms);
 }
 
-function order(term) {
-    return head(term);
-}
-
-function coeff(term) {
-    return head(tail(term));
-}
-
+// the polynomial package
 function install_polynomial_package() {
+    // imported functions from sparse and dense packages
+    function make_sparse_poly(variable, terms) {
+        return get("make_sparse_poly", "sparse")(variable, terms);
+    }
+    function make_dense_poly(variable, terms) {
+        return get("make_dense_poly", "dense")(variable, terms);
+    }
+
     // internal functions
-    // representation of poly
-    function make_poly(variable, term_list) {
-        return pair(variable, term_list);
+    function make_term(order, coeff) {
+        return list(order, coeff);
+    }
+    function order(term) {
+        return head(term);
+    }
+    function coeff(term) {
+        return head(tail(term));
     }
     function variable(p) { return head(p); }
     function term_list(p) { return tail(p); }
+    
+    function is_variable(x) { return is_string(x); }
 
+    function is_same_variable(v1, v2) {
+        return is_variable(v1) && is_variable(v2) && v1 === v2;
+    }
+    function first_term(tl) {
+        return apply_generic("first_term", list(tl));
+    }
+    function rest_terms(tl) {
+        return apply_generic("rest_terms", list(tl));
+    }
+    function is_empty_termlist(tl) {
+        return apply_generic("is_empty_termlist", list(tl));
+    }
+    function the_empty_termlist(tl) {
+        return get("the_empty_termlist", type_tag(tl))();
+    }
+    function adjoin_term(term, term_list) {
+        const fun = get("adjoin_term", type_tag(term_list));
+        return !is_undefined(fun)
+            ? fun(term, contents(term_list))
+            : error(list(term_list), "no function for these types - adjoin_term");
+    }
+
+    function make_poly(variable, term_list) {
+        return pair(variable, term_list);
+    }
+    
     function add_poly(p1, p2) {
         return is_same_variable(variable(p1), variable(p2))
                ? make_poly(variable(p1),
@@ -72,8 +100,7 @@ function install_polynomial_package() {
                    : order(t1) < order(t2)
                    ? adjoin_term(t2, add_terms(L1, rest_terms(L2)))
                    : adjoin_term(make_term(order(t1),
-                                           add(coeff(t1),
-                                               coeff(t2))),
+                                           coeff(t1) + coeff(t2)),
                                  add_terms(rest_terms(L1),
                                            rest_terms(L2)));
         }
@@ -87,34 +114,21 @@ function install_polynomial_package() {
                : error(list(p1, p2), "polys not in same var -- mul_poly");
     }
 
-    function sub_poly(p1, p2) {
-        return add_poly(p1, negate_poly(p2));
-    }
-
-    function negate_poly(p) {
-        return tail(make_polynomial("x", map(negate_term, tail(p))));
-    }
-
-    function negate_term(t) {
-        const n = -tail(coeff(t));
-        return make_term(order(t), make_javascript_number(n));
-    }
-
     function mul_terms(L1, L2) {
         return is_empty_termlist(L1)
-               ? the_empty_termlist
+               ? the_empty_termlist(L1)
                : add_terms(mul_term_by_all_terms(
                                      first_term(L1), L2),
                            mul_terms(rest_terms(L1), L2));
     }
     function mul_term_by_all_terms(t1, L) {
         if (is_empty_termlist(L)) {
-            return the_empty_termlist;
+            return the_empty_termlist(L);
         } else {
             const t2 = first_term(L);
             return adjoin_term(
                        make_term(order(t1) + order(t2),
-                                 mul(coeff(t1), coeff(t2))),
+                                 coeff(t1) * coeff(t2)),
                        mul_term_by_all_terms(t1, rest_terms(L)));
         }
     }
@@ -125,147 +139,139 @@ function install_polynomial_package() {
     }
     put("add", list("polynomial", "polynomial"),
         (p1, p2) => tag(add_poly(p1, p2)));
-    put("sub", list("polynomial", "polynomial"),
-        (p1, p2) => tag(sub_poly(p1, p2)));
     put("mul", list("polynomial", "polynomial"),
         (p1, p2) => tag(mul_poly(p1, p2)));
-    put("make", "polynomial",
-        (variable, terms) => tag(make_poly(variable, terms)));
+    put("make_sparse", "polynomial",
+        (variable, terms) => tag(make_sparse_poly(variable, terms)));
+    put("make_dense", "polynomial",
+        (variable, terms) => tag(make_dense_poly(variable, terms)));
 
     return "done";
 }
 
-function install_sparse_term_list_package() {
-    // representation of terms and term lists
-    function adjoin_term(term, term_list) {
-        return is_equal_to_zero(coeff(term))
-               ? term_list
-               : pair(term, term_list);
-    }
+// sparse representation of polynomials
+function install_sparse_package() {
+    // internal functions
+    const the_empty_termlist = null;
     function first_term(term_list) {
         return head(term_list);
     }
     function rest_terms(term_list) {
         return tail(term_list);
     }
-
+    function is_empty_termlist(term_list) {
+        return is_null(term_list);
+    }
+    function coeff(term) {
+        head(tail(term));
+    }
+    function adjoin_term(term, term_list) {
+        return coeff(term) === 0
+               ? term_list
+               : pair(term, term_list);
+    }
+    function make_sparse_poly(variable, term_list) {
+        return pair(variable, term_list);
+    }
+    
+    // interface to the rest of the system
     function tag(term_list) {
         return attach_tag("sparse", term_list);
     }
 
-    put("first_term", "sparse", first_term);
-    put("rest_terms", "sparse", term_list => tag(rest_terms(term_list)));
-    put("adjoin_term", "sparse", (term, term_list) => tag(adjoin_term(term, term_list)));
-    put("make", "sparse", () => tag(list()));
+    put("first_term", list("sparse"), tl => first_term(tl));
+    put("rest_terms", list("sparse"), tl => tag(rest_terms(tl)));
+    put("is_empty_termlist", list("sparse"), tl => is_empty_termlist(tl));
+    put("the_empty_termlist", "sparse", () => tag(the_empty_termlist));
+    put("adjoin_term", "sparse", (t, tl) => tag(adjoin_term(t, tl)));
+    put("make_sparse_poly", "sparse", (variable, terms) => make_sparse_poly(variable, tag(terms)));
+    
+    return "done";
 }
 
-function install_dense_term_list_package() {
-    // representation of terms and term lists
-    function adjoin_term(term, term_list) {
-        return is_equal_to_zero(coeff(term))
-               ? term_list
-               : pair(coeff(term), term_list);
+// dense representation of polynomials
+function install_dense_package() {
+    // internal functions
+    const the_empty_termlist = null;
+    function make_term(order, coeff) {
+        return list(order, coeff);
     }
     function first_term(term_list) {
-        return make_term(head(term_list), length(term_list)-1);
+        return make_term(length(term_list) - 1, head(term_list));
     }
     function rest_terms(term_list) {
         return tail(term_list);
     }
-
+    function is_empty_termlist(term_list) {
+        return is_null(term_list);
+    }
+    function order(term) {
+        head(term);
+    }
+    function coeff(term) {
+        head(tail(term));
+    }
+    function adjoin_term(term, term_list) {
+        return coeff(term) === 0
+               ? term_list
+               : order(term) === length(term_list)
+               ? pair(coeff(term), term_list)
+               : adjoin_term(term, pair(0, term_list));
+    }
+    function make_dense_poly(variable, term_list) {
+        return pair(variable, term_list);
+    }
+    
+    // interface to the rest of the system
     function tag(term_list) {
         return attach_tag("dense", term_list);
     }
 
-    put("first_term", "dense", first_term);
-    put("rest_terms", "dense", term_list => tag(rest_terms(term_list)));
-    put("adjoin_term", "dense", (term, term_list) => tag(adjoin_term(term, term_list)));
-}
-
-// javascript number package
-function install_javascript_number_package() {
-    function tag(x) {
-        return attach_tag("javascript_number", x);
-    }
-    put("add", list("javascript_number", "javascript_number"),
-        (x, y) => tag(x + y));
-    put("sub", list("javascript_number", "javascript_number"),
-        (x, y) => tag(x - y));
-    put("mul", list("javascript_number", "javascript_number"),
-        (x, y) => tag(x * y));
-    put("div", list("javascript_number", "javascript_number"),
-        (x, y) => tag(x / y));
-    put("is_equal_to_zero", list("javascript_number"), x => x === 0);
-    put("make", "javascript_number", x => tag(x));
-
+    put("first_term", list("dense"), tl => first_term(tl));
+    put("rest_terms", list("dense"), tl => tag(rest_terms(tl)));
+    put("is_empty_termlist", list("dense"), tl => is_empty_termlist(tl));
+    put("the_empty_termlist", "dense", () => tag(the_empty_termlist));
+    put("adjoin_term", "dense", (t, tl) => tag(adjoin_term(t, tl)));
+    put("make_dense_poly", "dense", (variable, terms) => make_dense_poly(variable, tag(terms)));
+    
     return "done";
 }
 
+// to keep things simple, I decided to use
+// primitive numbers to represent the polynomial's coefficients
+
+// generic functions
 function add(x, y) { return apply_generic("add", list(x, y)); }
-
 function sub(x, y) { return apply_generic("sub", list(x, y)); }
-
 function mul(x, y) { return apply_generic("mul", list(x, y)); }
-
 function div(x, y) { return apply_generic("div", list(x, y)); }
-
-function is_equal_to_zero(x) {
-    return apply_generic("is_equal_to_zero", list(x));
-}
-
-function make_javascript_number(n) {
-    return get("make", "javascript_number")(n);
-}
-
-function make_polynomial(variable, terms) {
-    return get("make", "polynomial")(variable, terms);
-}
-
-function is_variable(x) { return is_string(x); }
-
-function is_same_variable(v1, v2) {
-    return is_variable(v1) && is_variable(v2) && v1 === v2;
-}
-
-function adjoin_term(term, term_list) { // FINISH THIS
-    return get("adjoin_term", type_tag(term_list), term, term_list);
-}
-
-function first_term(term_list) {
-    return apply_generic("first_term", list(term_list));
-}
-
-function rest_terms(term_list) {
-    return apply_generic("rest_terms", list(term_list));
-}
 
 function attach_tag(type_tag, contents) {
     if (is_number(contents)) {
         return pair("javascript_number", contents);
+    } else {
+        return pair(type_tag, contents);
     }
-    return pair(type_tag, contents);
 }
 
 function type_tag(datum) {
     if (is_number(datum)) {
         return "javascript_number";
-    }
-    if (is_pair(datum)) {
+    } else if (is_pair(datum)) {
         return head(datum);
+    } else {
+        return error(datum, "bad tagged datum -- type_tag");
     }
-
-    return error(datum, "bad tagged datum -- type_tag");
 }
 
 function contents(datum) {
     if (is_number(datum)) {
         return datum;
-    }
-    if (is_pair(datum)) {
+    } else if (is_pair(datum)) {
         return tail(datum);
+    } else {
+        return error(datum, "bad tagged datum -- contents");
     }
-
-    return error(datum, "bad tagged datum -- contents");
 }
 
 function apply(fun, args) {
@@ -279,14 +285,6 @@ function apply_generic(op, args) {
     return !is_undefined(fun)
            ? apply_in_underlying_javascript(fun, map(contents, args))
            : error(list(op, type_tags), "no method for these types -- apply_generic");
-}
-
-function gcd(a, b) {
-    return b === 0 ? a : gcd(b, a % b);
-}
-
-function square(x) {
-    return x * x;
 }
 
 // operation_table, put and get
