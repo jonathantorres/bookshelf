364053f5d304598bf48ce68cf88715087a62ac10
Jonathan Torres <jonathantorres41@gmail.com>
2023-12-22 18:40:41 -0500

    sicp-js: implement answer for 2.92

--- sicp-js/ch2/2.92.js
+++ sicp-js/ch2/2.92.js
@@ -4,203 +4,113 @@ import {
     map, apply_in_underlying_javascript, math_sqrt, math_atan
 } from 'sicp';
 
-const operation_table = make_table();
-const get = operation_table("lookup");
-const put = operation_table("insert");
+// to keep things simple, the coefficients are represented as primitive numbers
+const p1 = make_poly("x", list(make_term(100, 1), make_term(2, 2), make_term(0, 1)));
+const p2 = make_poly("y", list(make_term(55, 5), make_term(3, 2), make_term(1, 1)));
 
-install_javascript_number_package();
-install_polynomial_package();
+display_list(add_poly(p1, p2));
+display_list(mul_poly(p1, p2));
 
-
-// TODO: Implement this
-
-
-function install_polynomial_package() {
-    // internal functions
-    // representation of poly
-    function make_poly(variable, term_list) {
-        return pair(variable, term_list);
+// rewrittern polynomial addition function
+function add_poly(p1, p2) {
+    const v1 = variable(p1);
+    const v2 = variable(p2);
+    const t1 = term_list(p1);
+    const t2 = term_list(p2);
+    
+    if (is_same_variable(v1, v2)) {
+        return make_poly(v1, add_terms(t1, t2));
+    } else if (is_before(v1, v2)) {
+        return add_poly(p1, append_poly(v1, p2));
+    } else {
+        return add_poly(append_poly(v2, p1), p2);
     }
-    function variable(p) { return head(p); }
-    function term_list(p) { return tail(p); }
-
-    // representation of terms and term lists
-    function adjoin_term(term, term_list) {
-        return is_equal_to_zero(coeff(term))
-               ? term_list
-               : pair(coeff(term), term_list);
-    }
-    const the_empty_termlist = null;
-    function first_term(term_list) {
-        return make_term(head(term_list), length(term_list)-1);
-    }
-    function rest_terms(term_list) {
-        return tail(term_list);
-    }
-    function is_empty_termlist(term_list) {
-        return is_null(term_list);
-    }
-    function make_term(order, coeff) {
-        return list(order, coeff);
-    }
-    function order(term) {
-        return head(term);
-    }
-    function coeff(term) {
-        return head(tail(term));
-    }
-
-    function add_poly(p1, p2) {
-        return is_same_variable(variable(p1), variable(p2))
-               ? make_poly(variable(p1),
-                           add_terms(term_list(p1),
-                                     term_list(p2)))
-               : error(list(p1, p2), "polys not in same var -- add_poly");
-    }
-
-    function add_terms(L1, L2) {
-        if (is_empty_termlist(L1)) {
-          return L2;
-        } else if (is_empty_termlist(L2)) {
-          return L1;
-        } else {
-            const t1 = first_term(L1);
-            const t2 = first_term(L2);
-            return order(t1) > order(t2)
-                   ? adjoin_term(t1, add_terms(rest_terms(L1), L2))
-                   : order(t1) < order(t2)
-                   ? adjoin_term(t2, add_terms(L1, rest_terms(L2)))
-                   : adjoin_term(make_term(order(t1),
-                                           add(coeff(t1),
-                                               coeff(t2))),
-                                 add_terms(rest_terms(L1),
-                                           rest_terms(L2)));
-        }
-    }
-
-    function mul_poly(p1, p2) {
-        return is_same_variable(variable(p1), variable(p2))
-               ? make_poly(variable(p1),
-                           mul_terms(term_list(p1),
-                                     term_list(p2)))
-               : error(list(p1, p2), "polys not in same var -- mul_poly");
-    }
-
-    function sub_poly(p1, p2) {
-        return add_poly(p1, negate_poly(p2));
-    }
-
-    function negate_poly(p) {
-        return tail(make_polynomial("x", map(negate_term, tail(p))));
-    }
-
-    function negate_term(t) {
-        const n = -tail(coeff(t));
-        return make_term(order(t), make_javascript_number(n));
-    }
-
-    function mul_terms(L1, L2) {
-        return is_empty_termlist(L1)
-               ? the_empty_termlist
-               : add_terms(mul_term_by_all_terms(
-                                     first_term(L1), L2),
-                           mul_terms(rest_terms(L1), L2));
-    }
-    function mul_term_by_all_terms(t1, L) {
-        if (is_empty_termlist(L)) {
-            return the_empty_termlist;
-        } else {
-            const t2 = first_term(L);
-            return adjoin_term(
-                       make_term(order(t1) + order(t2),
-                                 mul(coeff(t1), coeff(t2))),
-                       mul_term_by_all_terms(t1, rest_terms(L)));
-        }
-    }
-    function div_poly(p1, p2) {
-        if (is_same_variable(variable(p1), variable(p2))) {
-            return map(term_list => make_poly(variable(p1), term_list),
-                div_terms(term_list(p1), term_list(p2)));
-        }
-        return error("Polynomials not in same var - div_poly", list(p1, p2));
-    }
-
-    function div_terms(L1, L2) {
-        if (is_empty_termlist(L1)) {
-            return list(the_empty_termlist, the_empty_termlist);
-        } else {
-            const t1 = first_term(L1);
-            const t2 = first_term(L2);
-            if (order(t2) > order(t1)) {
-                return list(the_empty_termlist, L1);
-            } else {
-                const new_c = div(coeff(t1), coeff(t2));
-                const new_o = order(t1) - order(t2);
-                const term = list(new_o, new_c);
-                const rest_of_result = div_terms(
-                    sub_poly(L1, mul_terms(adjoin_term(term, the_empty_termlist), L2)),
-                    L2
-                );
-                return list(adjoin_term(term, head(rest_of_result)), tail(rest_of_result));
-            }
-        }
-    }
-
-    // interface to rest of the system
-    function tag(p) {
-        return attach_tag("polynomial", p);
-    }
-    put("add", list("polynomial", "polynomial"),
-        (p1, p2) => tag(add_poly(p1, p2)));
-    put("sub", list("polynomial", "polynomial"),
-        (p1, p2) => tag(sub_poly(p1, p2)));
-    put("mul", list("polynomial", "polynomial"),
-        (p1, p2) => tag(mul_poly(p1, p2)));
-    put("div", list("polynomial", "polynomial"),
-        (p1, p2) => tag(div_poly(p1, p2)));
-    put("make", "polynomial",
-        (variable, terms) => tag(make_poly(variable, terms)));
-
-    return "done";
 }
 
-// javascript number package
-function install_javascript_number_package() {
-    function tag(x) {
-        return attach_tag("javascript_number", x);
+// rewritten polynomial multiplication function
+function mul_poly(p1, p2) {
+    const v1 = variable(p1);
+    const v2 = variable(p2);
+    const t1 = term_list(p1);
+    const t2 = term_list(p2);
+    
+    if (is_same_variable(v1, v2)) {
+        return make_poly(v1, mul_terms(t1, t2));
+    } else if (is_before(v1, v2)) {
+        return mul_poly(p1, append_poly(v1, p2));
+    } else {
+        return mul_poly(append_poly(v2, p1), p2);
     }
-    put("add", list("javascript_number", "javascript_number"),
-        (x, y) => tag(x + y));
-    put("sub", list("javascript_number", "javascript_number"),
-        (x, y) => tag(x - y));
-    put("mul", list("javascript_number", "javascript_number"),
-        (x, y) => tag(x * y));
-    put("div", list("javascript_number", "javascript_number"),
-        (x, y) => tag(x / y));
-    put("is_equal_to_zero", list("javascript_number"), x => x === 0);
-    put("make", "javascript_number", x => tag(x));
-
-    return "done";
 }
 
-function add(x, y) { return apply_generic("add", list(x, y)); }
-
-function sub(x, y) { return apply_generic("sub", list(x, y)); }
-
-function mul(x, y) { return apply_generic("mul", list(x, y)); }
-
-function div(x, y) { return apply_generic("div", list(x, y)); }
-
-function is_equal_to_zero(x) {
-    return apply_generic("is_equal_to_zero", list(x));
+function adjoin_term(term, term_list) {
+    if (is_number(coeff(term)) && coeff(term) === 0) {
+        return term_list;
+    } else if (is_empty_termlist(term_list)) {
+        return term_list;
+    } else if (order(head(term_list)) > order(term)) {
+        return pair(head(term_list), adjoin_term(term, tail(term_list)));
+    } else if (order(head(term_list)) === order(term)) {
+        return adjoin_term(
+                make_term(order(term), coeff(term) + coeff(head(term_list))),
+                tail(term_list));
+    } else {
+        return pair(term, term_list);
+    }
 }
 
-function make_javascript_number(n) {
-    return get("make", "javascript_number")(n);
+
+function is_before(v1, v2) {
+    return v1 < v2;
 }
 
-function make_polynomial(variable, terms) {
-    return get("make", "polynomial")(variable, terms);
+function append_poly(variable, num) {
+    return make_poly(variable, adjoin_term(make_term(0, num), the_empty_termlist));
+}
+
+// adding terms
+function add_terms(L1, L2) {
+    if (is_empty_termlist(L1)) {
+      return L2;
+    } else if (is_empty_termlist(L2)) {
+      return L1;
+    } else {
+        const t1 = first_term(L1);
+        const t2 = first_term(L2);
+        return order(t1) > order(t2)
+               ? adjoin_term(t1, add_terms(rest_terms(L1), L2))
+               : order(t1) < order(t2)
+               ? adjoin_term(t2, add_terms(L1, rest_terms(L2)))
+               : adjoin_term(make_term(order(t1),
+                                       coeff(t1) + coeff(t2)),
+                             add_terms(rest_terms(L1),
+                                       rest_terms(L2)));
+    }
+}
+
+// multiplying terms
+function mul_terms(L1, L2) {
+    return is_empty_termlist(L1)
+           ? the_empty_termlist
+           : add_terms(mul_term_by_all_terms(
+                                 first_term(L1), L2),
+                       mul_terms(rest_terms(L1), L2));
+}
+
+function mul_term_by_all_terms(t1, L) {
+    if (is_empty_termlist(L)) {
+        return the_empty_termlist;
+    } else {
+        const t2 = first_term(L);
+        return adjoin_term(
+                   make_term(order(t1) + order(t2),
+                             coeff(t1) * coeff(t2)),
+                   mul_term_by_all_terms(t1, rest_terms(L)));
+    }
+}
+
+function make_poly(variable, term_list) {
+    return pair(variable, term_list);
 }
 
 function is_variable(x) { return is_string(x); }
@@ -209,101 +119,25 @@ function is_same_variable(v1, v2) {
     return is_variable(v1) && is_variable(v2) && v1 === v2;
 }
 
-function attach_tag(type_tag, contents) {
-    if (is_number(contents)) {
-        return pair("javascript_number", contents);
-    }
-    return pair(type_tag, contents);
+const the_empty_termlist = null;
+function variable(p) { return head(p); }
+function term_list(p) { return tail(p); }
+
+function first_term(term_list) {
+    return head(term_list);
 }
-
-function type_tag(datum) {
-    if (is_number(datum)) {
-        return "javascript_number";
-    }
-    if (is_pair(datum)) {
-        return head(datum);
-    }
-
-    return error(datum, "bad tagged datum -- type_tag");
+function rest_terms(term_list) {
+    return tail(term_list);
 }
-
-function contents(datum) {
-    if (is_number(datum)) {
-        return datum;
-    }
-    if (is_pair(datum)) {
-        return tail(datum);
-    }
-
-    return error(datum, "bad tagged datum -- contents");
+function is_empty_termlist(term_list) {
+    return is_null(term_list);
 }
-
-function apply(fun, args) {
-    return apply_in_underlying_javascript(fun, args);
+function make_term(order, coeff) {
+    return list(order, coeff);
 }
-
-function apply_generic(op, args) {
-    const type_tags = map(type_tag, args);
-    const fun = get(op, type_tags);
-
-    return !is_undefined(fun)
-           ? apply_in_underlying_javascript(fun, map(contents, args))
-           : error(list(op, type_tags), "no method for these types -- apply_generic");
+function order(term) {
+    return head(term);
 }
-
-function gcd(a, b) {
-    return b === 0 ? a : gcd(b, a % b);
-}
-
-function square(x) {
-    return x * x;
-}
-
-// operation_table, put and get
-// from chapter 3 (section 3.3.3)
-function assoc(key, records) {
-    return is_null(records)
-           ? undefined
-           : equal(key, head(head(records)))
-           ? head(records)
-           : assoc(key, tail(records));
-}
-
-function make_table() {
-    const local_table = list("*table*");
-    function lookup(key_1, key_2) {
-        const subtable = assoc(key_1, tail(local_table));
-        if (is_undefined(subtable)) {
-            return undefined;
-        } else {
-            const record = assoc(key_2, tail(subtable));
-            return is_undefined(record)
-                   ? undefined
-                   : tail(record);
-        }
-    }
-    function insert(key_1, key_2, value) {
-        const subtable = assoc(key_1, tail(local_table));
-        if (is_undefined(subtable)) {
-            set_tail(local_table,
-                     pair(list(key_1, pair(key_2, value)),
-                          tail(local_table)));
-        } else {
-            const record = assoc(key_2, tail(subtable));
-            if (is_undefined(record)) {
-                set_tail(subtable,
-                         pair(pair(key_2, value), tail(subtable)));
-            } else {
-                set_tail(record, value);
-            }
-        }
-    }
-    function dispatch(m) {
-        return m === "lookup"
-               ? lookup
-               : m === "insert"
-               ? insert
-               : error(m, "unknown operation -- table");
-    }
-    return dispatch;
+function coeff(term) {
+    return head(tail(term));
 }
