4477bc93da21c34978bfaff0c61dbc40449831ba
Jonathan Torres <jonathantorres41@gmail.com>
2023-12-25 20:04:12 -0500

    sicp-js: update answer to 3.4

--- sicp-js/ch3/3.4.js
+++ sicp-js/ch3/3.4.js
@@ -12,9 +12,8 @@ display(acc("something", "withdraw")(40));
 display(acc("something", "withdraw")(40));
 display(acc("something", "withdraw")(40));
 
-function make_account(balance, acc_pass) {
-    let pass = "";
-    let pass_retry = 0;
+function make_account(balance, password) {
+    let incorrect_count = 0;
     function withdraw(amount) {
         if (balance >= amount) {
             balance = balance - amount;
@@ -31,20 +30,21 @@ function make_account(balance, acc_pass) {
         return "calling the cops now";
     }
     function dispatch(p, m) {
-        if (acc_pass !== p) {
-            pass_retry++;
-            if (pass_retry > 7) {
-                return call_the_cops;
-            }
-            return p => {
+        if (password !== p) {
+            return x => {
+                incorrect_count = incorrect_count + 1;
+                if (incorrect_count > 7) {
+                    return call_the_cops();
+                }
                 return "Incorrect password";
-            }
-        }
-        return m === "withdraw"
+            };
+        } else {
+            return m === "withdraw"
                ? withdraw
                : m === "deposit"
                ? deposit
                : error(m, "unknown request -- make_account");
+        }
     }
     return dispatch;
 }
