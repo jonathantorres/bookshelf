d1675878dda989216a115c8df1aafde42e59f55c
Jonathan Torres <jonathantorres41@gmail.com>
2023-12-27 01:57:08 -0500

    sicp-js: update answer to 3.8

--- sicp-js/ch3/3.8.js
+++ sicp-js/ch3/3.8.js
@@ -1,14 +1,14 @@
 import { display } from 'sicp';
 
-const f = makef(1/2);
+const f = makef();
 
 display(f(0) + f(1));
-display(f(1) + f(0));
 
-function makef(i) {
-    return x => {
-        i = x-i;
-        return i;
-    }
+function makef() {
+    let last = 0;
+    return (n) => {
+        const tmp = last;
+        last = n;
+        return tmp;
+    };
 }
-
