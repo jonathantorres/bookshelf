590dd34dfb32b55d31a6c89ace0eb2438d5c3d26
Jonathan Torres <jonathantorres41@gmail.com>
2024-01-21 20:10:10 -0500

    sicp: updates to use guile's implementation of streams and add a few useful procedures

--- sicp/ch3/streams.scm
+++ sicp/ch3/streams.scm
@@ -1,11 +1,8 @@
-(define the-empty-stream '())
-(define stream-null? null?)
+; guile's implementation of streams
+(use-modules (srfi srfi-41))
 
-(define (cons-stream a b)
-  (cons a (delay b)))
-
-(define (stream-car stream) (car stream))
-(define (stream-cdr stream) (force (cdr stream)))
+(define the-empty-stream stream-null)
+(define-syntax cons-stream (identifier-syntax stream-cons))
 
 (define (stream-for-each proc s)
   (if (stream-null? s)
@@ -18,11 +15,13 @@
       (stream-car s)
       (stream-ref (stream-cdr s) (- n 1))))
 
-(define (stream-map proc s)
-  (if (stream-null? s)
-      the-empty-stream
-      (cons-stream (proc (stream-car s))
-                   (stream-map proc (stream-cdr s)))))
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+    the-empty-stream
+    (cons-stream
+      (apply proc (map stream-car argstreams))
+      (apply stream-map
+             (cons proc (map stream-cdr argstreams))))))
 
 (define (stream-enumerate-interval low high)
   (if (> low high)
@@ -38,6 +37,12 @@
                       (stream-filter pred
                                      (stream-cdr stream))))
         (else (stream-filter pred (stream-cdr stream)))))
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (mul-streams s1 s2)
+  (stream-map * s1 s2))
 
 (define (display-stream s)
   (display "( ")
