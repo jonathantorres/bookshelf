ea96edee3e89ef94498f80e6b40f30ec5b57058d
Jonathan Torres <jonathantorres41@gmail.com>
2024-04-27 01:53:51 -0400

    kr: updates on 1-3, 1-4 and 1-5

--- kr/ch1/1-3.c
+++ kr/ch1/1-3.c
@@ -6,13 +6,13 @@ int main(void)
     int lower, upper, step;
 
     lower = 0;
-    upper  = 300;
-    step = 20;
-    fahr = lower;
+    upper = 300;
+    step  = 20;
+    fahr  = lower;
 
     printf("Fahrenheit\tCelsius\n");
     while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr-32.0);
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
         printf("%3.0f\t\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
