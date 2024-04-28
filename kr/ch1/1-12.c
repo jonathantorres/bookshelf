29650589e1defc37139407e3d66b024e8c545b8c
Jonathan Torres <jonathantorres41@gmail.com>
2024-04-28 01:01:33 -0400

    kr: updates on 1-11, 1-12 and 1-13

--- kr/ch1/1-12.c
+++ kr/ch1/1-12.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
+#define IN  1
+#define OUT 0
+
 int main(void)
 {
     int c;
+    int state = OUT;
+
     while ((c = getchar()) != EOF) {
-        if (c == ' ') {
-            printf("\n");
+        if (c == ' ' || c == '\n' || c == '\t') {
+            if (state != OUT) {
+                putchar('\n');
+            }
+            state = OUT;
         } else {
             putchar(c);
+            state = IN;
         }
     }
+
     return 0;
 }
--- kr/ch1/1-13.c
+++ kr/ch1/1-13.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
 
+#define WLEN 1024
+
 int main(void)
 {
     int c;
-    int index = 0;
-    int word_lens[50] = { 0 };
-    int cur_len = 0;
+    int wl = 0;
+    int j  = 0;
+    int word_lengths[WLEN];
+
+    for (int i = 0; i < WLEN; i++) {
+        word_lengths[i] = 0;
+    }
 
     while ((c = getchar()) != EOF) {
-        if (c == ' ') {
-            word_lens[index] = cur_len;
-            cur_len = 0;
-            index++;
+        if (c == ' ' || c == '\n' || c == '\t') {
+            word_lengths[j++] = wl;
+            wl                = 0;
+        } else if (c == '.' || c == ',') {
+            // ignore periods and commas
         } else {
-            cur_len++;
-        }
-
-        if (c == '\n') {
-            word_lens[index] = cur_len-1;
-            cur_len = 0;
-            index++;
-            for (int i = 0; i < index; i++) {
-                for (int j = 0; j < word_lens[i]; j++) {
-                    printf("-");
-                }
-                printf("\n");
-            }
-            index = 0;
+            ++wl;
         }
     }
+
+    // print histogram
+    for (int i = 0; i < WLEN; i++) {
+        if (word_lengths[i] > 0) {
+            for (int j = 0; j < word_lengths[i]; j++) {
+                putchar('-');
+            }
+            printf("\n");
+        }
+    }
+
     return 0;
 }
