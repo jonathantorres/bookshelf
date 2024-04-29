7e7a05255a1110ef0a9f2fad00f5fee51ea3be90
Jonathan Torres <jonathantorres41@gmail.com>
2024-04-29 08:42:51 -0400

    kr: updates on 1-16 and 1-17

--- kr/ch1/1-16.c
+++ kr/ch1/1-16.c
@@ -2,10 +2,9 @@
 
 #define MAXLINE 1000
 
-int get_line(char line[], int maxline);
+int getline(char line[], int maxline);
 void copy(char to[], char from[]);
 
-// print longest input line
 int main(void)
 {
     int len;
@@ -14,29 +13,35 @@ int main(void)
     char longest[MAXLINE];
 
     max = 0;
-    while ((len = get_line(line, MAXLINE)) > 0) {
+
+    while ((len = getline(line, MAXLINE)) > 0) {
         if (len > max) {
             max = len;
             copy(longest, line);
         }
     }
+
     if (max > 0) {
-        printf("%s\n", longest);
+        printf("%s", longest);
+        printf("%d\n", max);
     }
+
     return 0;
 }
 
-int get_line(char s[], int lim)
+int getline(char s[], int lim)
 {
     int c, i;
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
         s[i] = c;
     }
+
     if (c == '\n') {
         s[i] = c;
         ++i;
     }
+
     s[i] = '\0';
 
     return i;
@@ -44,7 +49,9 @@ int get_line(char s[], int lim)
 
 void copy(char to[], char from[])
 {
-    int i = 0;
+    int i;
+
+    i = 0;
     while ((to[i] = from[i]) != '\0') {
         ++i;
     }
--- kr/ch1/1-17.c
+++ kr/ch1/1-17.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
 
-#define MAXLINE 1000
+#define MAXLINE  1000
+#define LONGLINE 80
 
-int get_line(char line[], int maxline);
+int getline(char line[], int maxline);
 
-// print input lines longer than 80 characters
 int main(void)
 {
     int len;
-    int max = 80;
     char line[MAXLINE];
 
-    while ((len = get_line(line, MAXLINE)) > 0) {
-        if (len > max) {
-            printf("%s\n", line);
+    while ((len = getline(line, MAXLINE)) > 0) {
+        if (len > LONGLINE) {
+            printf("%s", line);
         }
     }
+
     return 0;
 }
 
-int get_line(char s[], int lim)
+int getline(char s[], int lim)
 {
     int c, i;
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
         s[i] = c;
     }
+
     if (c == '\n') {
         s[i] = c;
         ++i;
     }
+
     s[i] = '\0';
 
     return i;
