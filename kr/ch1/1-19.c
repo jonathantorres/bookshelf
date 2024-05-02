03fab57427e4e4e17671ab92eb4ff201671bcacb
Jonathan Torres <jonathantorres41@gmail.com>
2024-05-02 08:41:14 -0400

    kr: updates on 1-19

--- kr/ch1/1-19.c
+++ kr/ch1/1-19.c
@@ -2,52 +2,53 @@
 
 #define MAXLINE 1000
 
-int get_line(char s[], int lim);
-void reverse(char s[], char rev[], int lim);
-void reset(char s[]);
+int getline(char line[], int maxline);
+void reverse(char s[], int size);
 
 int main(void)
 {
-    char line[MAXLINE];
-    char rev[MAXLINE];
-    char *revp = &rev[1]; // strip the leading newline
     int len;
+    char line[MAXLINE];
 
-    while ((len = get_line(line, MAXLINE)) > 0) {
-        reverse(line, rev, len);
-        printf("%s\n", revp);
+    for (int i = 0; i < MAXLINE; i++) {
+        line[i] = 0;
+    }
+
+    while ((len = getline(line, MAXLINE)) > 0) {
+        if (len > 0) {
+            reverse(line, len);
+            printf("%s", line);
+        }
     }
 
     return 0;
 }
 
-void reverse(char s[], char rev[], int lim)
+void reverse(char s[], int size)
 {
-    reset(rev);
-    for (int i = lim-1, j = 0; i >= 0; i--, j++) {
-        rev[j] = s[i];
+    int i, j;
+
+    for (i = 0, j = size - 2; i < j; ++i, --j) {
+        char t = s[i];
+        s[i]   = s[j];
+        s[j]   = t;
     }
 }
 
-int get_line(char s[], int lim)
+int getline(char s[], int lim)
 {
     int c, i;
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
         s[i] = c;
     }
+
     if (c == '\n') {
         s[i] = c;
         ++i;
     }
+
     s[i] = '\0';
 
     return i;
-}
-
-void reset(char s[])
-{
-    for (int i = 0; i < MAXLINE-1; i++) {
-        s[i] = '\0';
-    }
 }
