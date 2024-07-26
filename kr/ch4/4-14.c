64c1fca0895779f3d8f591d13a4650905078b4c2
Jonathan Torres <jonathantorres41@gmail.com>
2024-07-25 20:14:02 -0400

    kr: changes on 4-14

--- kr/ch4/4-14.c
+++ kr/ch4/4-14.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-#define swap(t,x,y) { t temp; temp = (x); (x) = (y); (y) = temp; }
+#define swap(t, x, y)                                                                              \
+    {                                                                                              \
+        t temp;                                                                                    \
+        temp = (x);                                                                                \
+        (x)  = (y);                                                                                \
+        (y)  = temp;                                                                               \
+    }
 
 int main(void)
 {
     int x = 3, y = 7;
+
     printf("x=%d, y=%d\n", x, y);
     swap(int, x, y);
     printf("x=%d, y=%d\n", x, y);
+
     return 0;
 }
