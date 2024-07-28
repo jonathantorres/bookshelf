daf01f793c66542789b32997298a9307f4bd4a93
Jonathan Torres <jonathantorres41@gmail.com>
2024-07-28 13:15:21 -0400

    kr: updates on 5-7

--- kr/ch5/5-7.c
+++ kr/ch5/5-7.c
@@ -1,52 +1,74 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAXLEN 1000   // max length of any input line
-#define MAXSTOR 5000  // max length of storage
+#define MAXLEN  1024
+#define MAXSTOR 5120
 
-int c_getline(char *s, int lim);
+int getline(char *s, int lim);
 int readlines(char *lineptr[], char *linestor, int maxlines);
+void writelines(char *lineptr[], int nlines);
+
+char *lineptr[MAXSTOR];
 
 int main(void)
 {
-    char line[MAXLEN];
-    while (c_getline(line, MAXLEN) > 0) {
-        printf("%s\n", line);
-    }
-    return 0;
-}
+    char linestor[MAXSTOR];
+    int nlines;
 
-int c_getline(char *s, int lim)
-{
-    int c;
-    int i = 0;
-    while (--lim > 0 && ((c = getchar()) != EOF && c != '\n')) {
-        i++;
-        *s++ = c;
+    while ((nlines = readlines(lineptr, linestor, MAXLEN)) >= 0) {
+        writelines(lineptr, nlines);
     }
-    if (c == '\n') {
-        i++;
-        *s++ = '\n';
-    }
-    *s = '\0';
-    return i;
+
+    return 0;
 }
 
 int readlines(char *lineptr[], char *linestor, int maxlines)
 {
     int len, nlines;
     char line[MAXLEN];
-    char *p = linestor;
+    char *p        = linestor;
     char *linestop = linestor + MAXSTOR;
-    nlines = 0;
-    while ((len = c_getline(line, MAXLEN)) > 0) {
+    nlines         = 0;
+
+    while ((len = getline(line, MAXLEN)) > 0) {
         if (nlines >= maxlines || p + len > linestop) {
-          return -1;
+            return -1;
         } else {
-            line[len-1] = '\0';   // delete newline
+            line[len - 1] = '\0';
             strcpy(p, line);
             lineptr[nlines++] = p;
+            p += len;
         }
     }
+
     return nlines;
+}
+
+void writelines(char *lineptr[], int nlines)
+{
+    int i;
+
+    for (i = 0; i < nlines; i++) {
+        printf("%s\n", lineptr[i]);
+    }
+}
+
+int getline(char *s, int lim)
+{
+    int c;
+    int i = 0;
+
+    while (--lim > 0 && ((c = getchar()) != EOF && c != '\n')) {
+        i++;
+        *s++ = c;
+    }
+
+    if (c == '\n') {
+        i++;
+        *s++ = '\n';
+    }
+
+    *s = '\0';
+
+    return i;
 }
