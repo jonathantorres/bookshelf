02896c20d064377756310a8099dc31ee6913827d
Jonathan Torres <jonathantorres41@gmail.com>
2024-09-28 03:25:12 -0400

    unp: chapter 11 exercises

---
+++ unp/ch11/11.8.c
@@ -0,0 +1,40 @@
+#include "unp.h"
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+    int listenfd, connfd;
+    socklen_t len, addrlen;
+    char buff[MAXLINE];
+    char host[NI_MAXHOST];
+    char serv[NI_MAXSERV];
+    time_t ticks;
+    struct sockaddr_storage cliaddr;
+
+    if (argc == 2) {
+        listenfd = Tcp_listen(NULL, argv[1], &addrlen);
+    } else if (argc == 3) {
+        listenfd = Tcp_listen(argv[1], argv[2], &addrlen);
+    } else {
+        err_quit("usage: daytimetcpsrv2 [ <host> ] <service or port>");
+    }
+
+    for (;;) {
+        len    = sizeof(cliaddr);
+        connfd = Accept(listenfd, (SA *)&cliaddr, &len);
+        printf("connection from %s\n", Sock_ntop((SA *)&cliaddr, len));
+
+        if (getnameinfo((SA *)&cliaddr, len, host, NI_MAXHOST, serv, NI_MAXSERV,
+                        NI_NUMERICHOST | NI_NUMERICSERV) == 0) {
+            printf("connection from: %s.%s\n", host, serv);
+        }
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+        Write(connfd, buff, strlen(buff));
+
+        Close(connfd);
+    }
+
+    return 0;
+}
